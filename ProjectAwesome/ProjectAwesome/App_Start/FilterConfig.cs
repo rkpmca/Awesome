f58888f84ca4212b1b79b2a955437c0a07381ff8
rkpmca@gmail.com <rkpmca@gmail.com>
2017-10-10 12:29:29 +0530

    Awesome project initial checkin

---
+++ ProjectAwesome/ProjectAwesome/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectAwesome
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
